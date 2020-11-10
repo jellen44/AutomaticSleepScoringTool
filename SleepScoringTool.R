b724819aa914c4613e309e8166fe557fd4faa030
jellen44 <34974716+jellen44@users.noreply.github.com>
2020-11-10 14:47:50 -0500

    Update SleepScoringTool.R

--- SleepScoringTool.R
+++ SleepScoringTool.R
@@ -1,6 +1,6 @@
 start_time <- Sys.time()
 
-#FOR THE USER
+#MANDATORY FOR THE USER
 
 #Please enter the file paths of the EEG sleep scoring files
 #These files should be your csv file of raw EEG data with the datapoints of each sleep epoch 
@@ -11,27 +11,45 @@ start_time <- Sys.time()
 #Unscored epochs can be left blank within the "Score" column.
 
 #Example of a file path: "/Users/YOURNAME/desktop/YOURFILE.csv"
-file_path1_eeg <- "/Users/jacobellen/desktop/eeg1exampledata.csv"
-file_path2_eeg <- "/Users/jacobellen/desktop/eeg2exampledata.csv" #NOTE: If not using two eeg files, there is no need to change this line,
+file_path1_eeg <- ""
+file_path2_eeg <- "" #NOTE: If not using two eeg files, there is no need to change this line,
 #but you must change the eeg_answer argument below to "One."
+#EXAMPLE PATH: "/Users/jacobellen/desktop/eeg1exampledata.csv"
 
 
 #File Path of the EMG File
 #This file should also be a csv file
-file_path_emg <- "/Users/jacobellen/desktop/emgexampledata.csv"
+file_path_emg <- ""
+#EXAMPLE PATH: "/Users/jacobellen/desktop/eeg1exampledata.csv"
 
 #File Path of the Output CSV File With All of the Predictions
-final_outputpath <- "/Users/jacobellen/desktop/finaloutput.csv"
+final_outputpath <- ""
+#EXAMPLE PATH: /Users/jacobellen/desktop/finaloutput.csv
 
 
 
-#OPTIONAL TUNING PARAMETERS:
+#ADDITIONAL (OPTIONAL) TUNING PARAMETERS:
 
 #ONE VS TWO EEGs
 
 #Would you like to use one or two EEGs?
 #If the user would only like to use one EEG, replace "Two" with "One" below
 eeg_answer <- "Two"
+
+
+#OPTION TO RUN THE SAME MODEL ON OTHER FILES OF THE SAME RAT
+#NOTE: You don't need any training data for additional files of same rat
+#Change the "No" to "Yes" Below
+#You can list the paths of as many additional files as you want, just add a comma and 
+#specify a new path in the for each file
+#These files do not need any "scoring" column with results, only the raw data for the EEGs and EMG.
+scoreotherfilesofsamerat <- "No"
+filepatheeg1_additionalfiles <- c("")
+filepatheeg2_additionalfiles <- c("")
+filepathemg_additionalfiles <- c("") 
+#EXAMPLE PATH: "/Users/jacobellen/desktop/eeg1exampledata_additionalfile.csv"
+#EXAMPLE OF USING MULTIPLE FILE PATHS: 
+#c("/Users/jacobellen/desktop/eeg1exampledata_additionalfile.csv", "/Users/jacobellen/desktop/eeg1exampledata_additionalfile2.csv")
 
 
 #OPTION TO INPUT EACH EEG AND EMG DATASET AS ONE LONG VECTOR AND SUBMIT SCORING DATA 
@@ -49,15 +67,14 @@ numberofdatapointsperepoch <- 1000
 scoringcolumn_filepath <- ""
 
 
-
 #EXTRACTING PROCESSED FILE
 
 #If you would like to get a copy of the frequency-domain data in csv form, input "Yes" below 
 #along with a file name and path to output it.
 #There's no to change this section if you don't want the transformed data file
 frequency_answer <- "No"
-file_output_path <- "/Users/YOURNAME/desktop/spectraldata.csv" #Example file path
-
+file_output_path <- ""
+#EXAMPLE PATH: "/Users/YOURNAME/desktop/spectraldata.csv"
 
 
 #SCRIPT 
@@ -254,8 +271,8 @@ ml.test <- ml %>%
 ml.test <- ml.test %>% column_to_rownames('index')
 
 #Warning produced if less than the training dataset is less than 5% REM or more than 12%
-if (length(which(ml.train$Score=="2"))/(length(ml.train$Score)) < .05) {
-  print("Warning: you have less than 5% REM in your training data, so you should add more REM to help the algorithm work properly")
+if (length(which(ml.train$Score=="2"))/(length(ml.train$Score)) < .10) {
+  print("Warning: you have less than 10% REM in your training data, so you should add more REM to help the algorithm work properly")
 }  
 
 ml.train <- as.matrix(ml.train)
@@ -298,8 +315,8 @@ model %>% compile(
 for (n in 1:5) {
 history <- model %>% fit(
   xtrain, ytrain,
-  epochs = 12, batch_size = 10, 
-  validation_split = 0.18
+  epochs = 100, batch_size = 10,
+  class_weight = list("0"=1,"1"=1, "2"=2)
 )
 plot(history)
 
@@ -332,27 +349,172 @@ finaloutput <- finaloutput %>% dplyr::select(-c(index))
 #titled "ModelPredictions" and an index column with the number of each epoch.
 write.csv(finaloutput, final_outputpath)
 
+
+
+#If you want to score the different files of the same rat using the model trained above
+if (scoreotherfilesofsamerat == "Yes") {
+  print("Analyzing Additional EEG Data")
+for (c in 1:(length(filepatheeg1_additionalfiles))) {
+  #Performing Spectral Analysis on First EEG Data
+  eeg1 <- fread(filepatheeg1_additionalfiles[c])
+  eeg1 <- as.data.frame(eeg1)
+  #eeg1 <- eeg1[1:20000,1:600]
+  if(eeg1[1,1]==1 & eeg1[2,1] ==2) {
+    eeg1 <- eeg1[, -1]
+  }
+  
+  scoringcolumn <- ""
+  if (vectorinput=="Yes") {
+    eeg1 <- as.matrix(eeg1)
+    eeg1 <- t(eeg1)
+    num <- length(eeg1)/numberofdatapointsperepoch
+    eeg1 <- matrix(eeg1, num, numberofdatapointsperepoch, byrow = T)
+    eeg1 <- as.data.frame(eeg1)
+    scoringcolumn <- "separate"
+  }
+  
+  
+  
+  if (is.data.frame(eeg1)==TRUE) {
+    eeg1 <- as.matrix(eeg1)
+  }
+  
+  fs <- matrix(0, nrow(eeg1), 125)
+  for (x in 1:nrow(eeg1)) {
+    print(paste0("Processing First EEG Data", x))
+    spectrum3 <- oce::pwelch(eeg1[x,], points = ncol(eeg1), plot=FALSE)
+    length(spectrum3$spec)
+    fs[x,(1:length(spectrum3$spec))] <- spectrum3$spec 
+  }
+  
+  
+  #Performing Spectral Analysis on Second EEG Data
+  if (eeg_answer=="Two") {
+    eeg2 <- fread(filepatheeg2_additionalfiles[c])
+    eeg2 <- as.data.frame(eeg2)
+    
+    if(eeg2[1,1]==1 & eeg2[2,1] ==2) {
+      eeg2 <- eeg2[, -1]
+    }
+    
+    if (vectorinput=="Yes") {
+      eeg2 <- as.matrix(eeg2)
+      eeg2 <- t(eeg2)
+      num2 <- length(eeg2)/numberofdatapointsperepoch
+      eeg2 <- matrix(eeg2, num, numberofdatapointsperepoch, byrow = T)
+      eeg2 <- as.data.frame(eeg2)
+    }
+    
+    if (is.data.frame(eeg2)==TRUE) {
+      eeg2 <- as.matrix(eeg2)
+    }
+    
+    
+    fs2 <- matrix(0, nrow(eeg2), 125)
+    for (x in 1:nrow(eeg2)) {
+      print(paste0("Processing Second EEG Data", x))
+      spectrum3 <- oce::pwelch(eeg2[x,], points = ncol(eeg2), plot=FALSE)
+      fs2[x,(1:length(spectrum3$spec))] <- spectrum3$spec 
+    }
+  }
+  
+  
+  # Feature Extraction from EEGs
+  delta <- as.vector(rowSums(fs[,1:4]))
+  theta <- as.vector(rowSums(fs[,4:7]))
+  upper_theta <- as.vector(rowSums(fs[,7:9]))
+  alpha <- as.vector(rowSums(fs[,8:12]))
+  beta <- as.vector(rowSums(fs[,13:30]))
+  lower_gamma <- as.vector(rowSums(fs[,30:50]))
+  medium_gamma <- as.vector(rowSums(fs[,50:75]))
+  high_gamma <- as.vector(rowSums(fs[,76:125]))
+  ratio_betadelta <- (beta/delta)
+  ratio_betalowergamma <- (beta/lower_gamma)
+  ratio_betamediumgamma <- (beta/medium_gamma)
+  ratio_betahighgamma <- (beta/high_gamma)
+  ratio_thetadelta <- (theta/delta)
+  ratio_thetagamma <- (theta/medium_gamma)
+  final <- as.data.frame(cbind(delta, theta,upper_theta,alpha, beta, lower_gamma, medium_gamma, 
+                               high_gamma, ratio_betadelta, ratio_betalowergamma, ratio_betahighgamma,
+                               ratio_thetadelta, ratio_thetagamma))
+  
+  
+  if (eeg_answer=="Two") {
+    delta2 <- as.vector(rowSums(fs2[,1:4]))
+    theta2 <- as.vector(rowSums(fs2[,4:7]))
+    upper_theta2 <- as.vector(rowSums(fs2[,7:9]))
+    alpha2 <- as.vector(rowSums(fs2[,8:12]))
+    beta2 <- as.vector(rowSums(fs2[,13:30]))
+    lower_gamma2 <- as.vector(rowSums(fs2[,30:50]))
+    medium_gamma2 <- as.vector(rowSums(fs2[,50:75]))
+    high_gamma2 <- as.vector(rowSums(fs2[,77:125]))
+    ratio_thetagamma2 <- (theta2/medium_gamma2)
+    ratio_betadelta2 <- (beta2/delta2)
+    ratio_betalowergamma2 <- (beta2/lower_gamma2)
+    ratio_betamediumgamma2 <- (beta2/medium_gamma2)
+    ratio_betahighgamma2 <- (beta2/high_gamma2)
+    ratio_thetadelta2 <- (theta2/delta2)
+    final <- as.data.frame(cbind(final, delta2, theta2,upper_theta2,alpha2, beta2, lower_gamma2, 
+                                 medium_gamma2, high_gamma2, ratio_betadelta2, ratio_betalowergamma2,
+                                 ratio_betamediumgamma2, ratio_betahighgamma2, ratio_thetadelta2))
+  }
+  
+  
+  #Processing EMG Data
+  emg <- fread(filepathemg_additionalfiles[c])
+  emg <- as.data.frame(emg)
+  if(emg[1,1]==1 & emg[2,1] ==2) {
+    emg <- emg[, -1]
+  }
+  
+  if (vectorinput=="Yes") {
+    emg <- as.matrix(emg)
+    emg <- t(emg)
+    emg <- length(emg)/numberofdatapointsperepoch
+    emg <- matrix(emg, num, numberofdatapointsperepoch, byrow = T)
+  }
+  
+  RMS <- as.vector(sqrt(abs(rowSums(emg))))
+  RMS_power <- as.vector(rowSums(abs(emg)))
+  
+  RMSplusgamma <- RMS+medium_gamma
+  RMSpowerplusgamma <- (RMS_power+medium_gamma)
+  
+  final <- as.data.frame(cbind(final, RMS, RMS_power, RMSplusgamma, RMSpowerplusgamma))
+  
+  final <- scale(final)
+  
+  iter <- c+1
+  if (frequency_answer=="Yes") {
+    write.csv(final, paste0(sub("\\..*", "", file_output_path), "_file", iter, ".csv"))
+  }
+  
+  
+  #Setting up the Algorithm
+  ml <- as.data.frame(final)
+  ml.train <- as.matrix(ml)
+
+  if (class(ml.train[1,1])=="character") {
+    class(ml.train) <- "numeric"
+  }
+  
+  #Generating Predictions
+  predictions_newfile <- predict_classes(model, ml.train) %>% as.vector()
+  predictions_newfile <- as.data.frame(predictions_newfile)
+  
+  #Finalizing Predictions and Formatting the Final Output
+  predictions.output1 <- as.data.frame(cbind(eeg1,predictions_newfile))
+  colnames(predictions.output1)[ncol(predictions.output1)] <- "ModelPredictions"
+  finaloutput_newfile <- predictions.output1
+
+
+#Outputting final CSV File with an additional column with all of the predictions (the last column
+#titled "ModelPredictions" and an index column with the number of each epoch.
+write.csv(finaloutput_newfile, paste0(sub("\\..*", "", final_outputpath), "_file", iter, ".csv"))
+
+}
+}
+
 end_time <- Sys.time()
 time <- end_time - start_time
 paste0("Script Finished in ", time, " Minutes")
-
-#FOR ME TO CHECK THAT ITS ACTUALLY WORKING
-
-#check <- as.data.frame(cbind(preds,finaloutput$ModelPredictions))
-#check <- na.omit(check)
-#check$V1 <- as.character(check$V1)
-#check$V1 <- as.numeric(check$V1)
-#check$V2 <- as.character(check$V2)
-#check$V2 <- as.numeric(check$V2)
-#check$V2 <- check$V2-1
-#sum(check$V1==check$V2)/(nrow(check))
-
-#check$V2 <- as.factor(check$V2)
-#check$V1 <- as.factor(check$V1)
-#confusionMatrix(check$V2, check$V1)
-
-
-
-
-
-
